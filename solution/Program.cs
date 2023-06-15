ddee06d0f831fb22b2dccbdb2fe46ebd6fac7f3d
Scarlett <sharashova.maria@yandex.ru>
2023-06-15 09:59:25 +0400

    solution 1

--- solution/Program.cs
+++ solution/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(15);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],3} ");
+        }
+       System.Console.WriteLine();      
+    }
+}
+
+void ChangeArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+            for (int k = j + 1; k < arr.GetLength(1); k++)
+                if (arr[i,j] < arr[i, k]) (arr[i, k], arr[i, j]) = (arr[i, j], arr[i,k]);    
+    
+}
+
+System.Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
